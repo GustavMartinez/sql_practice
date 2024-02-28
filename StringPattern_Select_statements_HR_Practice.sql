afb6a662963f3c7b02f978c979f07c1ad4b32b6e
Gustavo Martinez <115798702+GustavMartinez@users.noreply.github.com>
2024-02-28 04:33:00 -0300

    Update StringPattern_Select_statements_HR_Practice.sql

--- StringPattern_Select_statements_HR_Practice.sql
+++ StringPattern_Select_statements_HR_Practice.sql
@@ -28,7 +28,7 @@ SELECT F_NAME, L_NAME, DEP_ID
 FROM EMPLOYEES
 ORDER BY DEP_ID DESC, L_NAME DESC;
 
---Optional Query 2.3-- In SQL 2.2, use department name instead of department ID. Retrieve a list of employees ordered by department name, and within each department ordered alphabetically in descending order by last name.
+--Query 2.3-- In SQL 2.2, use department name instead of department ID. Retrieve a list of employees ordered by department name, and within each department ordered alphabetically in descending order by last name.
 
 SELECT D.DEP_NAME , E.F_NAME, E.L_NAME
 FROM EMPLOYEES as E, DEPARTMENTS as D
